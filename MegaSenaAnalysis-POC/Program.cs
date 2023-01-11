13a7406da60cb21c3f825f7c2c0dc5579e9495eb
Alan Jefferson Nascimento <alan.jefferson@outlook.com>
2023-01-11 09:03:02 -0300

    Initial commit

---
+++ MegaSenaAnalysis-POC/Program.cs
@@ -0,0 +1,13 @@
+﻿using HtmlAgilityPack;
+
+HttpClient client = new HttpClient();
+
+var response = await client.GetAsync("https://loterias.caixa.gov.br/Paginas/Download-Resultados.aspx");
+var pageContents = await response.Content.ReadAsStringAsync();
+
+HtmlDocument pageDocument = new HtmlDocument();
+pageDocument.LoadHtml(pageContents);
+
+var headlineText = pageDocument.DocumentNode.SelectSingleNode("(//div[contains(@class,'todosResultados')]//h3)[1]").InnerText;
+Console.WriteLine(headlineText);
+Console.ReadLine();
